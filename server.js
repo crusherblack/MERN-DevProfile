d3947369439efa410803da06cde7a6068f4c94c1
crusherblack <fadhildarma13@gmail.com>
2020-05-24 16:07:39 +0700

    production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,14 +10,20 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-//test server running
-app.get('/', (req, res) => res.send('Api Running'));
-
 // Definiskan route
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 //default port 5000
 const PORT = process.env.PORT || 5000;
