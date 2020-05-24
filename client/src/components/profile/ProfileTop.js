1e950ba26b4225e5f5b60325a16040368841173a
crusherblack <fadhildarma13@gmail.com>
2020-05-24 16:28:04 +0700

    fix social button if not available

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -21,31 +21,36 @@ const ProfileTop = ({
 					</a>
 				)}
 
-				{social.twitter && (
+				{social &&
+				social.twitter && (
 					<a href={social.twitter} target="_blank" rel="noopener noreferrer">
 						<i className="fab fa-twitter fa-2x" />
 					</a>
 				)}
 
-				{social.facebook && (
+				{social &&
+				social.facebook && (
 					<a href={social.facebook} target="_blank" rel="noopener noreferrer">
 						<i className="fab fa-facebook fa-2x" />
 					</a>
 				)}
 
-				{social.linkedin && (
+				{social &&
+				social.linkedin && (
 					<a href={social.linkedin} target="_blank" rel="noopener noreferrer">
 						<i className="fab fa-linkedin fa-2x" />
 					</a>
 				)}
 
-				{social.youtube && (
+				{social &&
+				social.youtube && (
 					<a href={social.youtube} target="_blank" rel="noopener noreferrer">
 						<i className="fab fa-youtube fa-2x" />
 					</a>
 				)}
 
-				{social.instagram && (
+				{social &&
+				social.instagram && (
 					<a href={social.instagram} target="_blank" rel="noopener noreferrer">
 						<i className="fab fa-instagram fa-2x" />
 					</a>
